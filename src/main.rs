a6c49bfb365a597b62c263a37767814d352e00d9
Vince <vincivenv@gmail.com>
2023-10-03 23:01:02 +0200

    process frames and save

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use ffmpeg::format::input;
 use ffmpeg::media::Type;
 use ffmpeg::software::scaling::{Context, Flags};
 use ffmpeg::util::frame::video::Video;
+use image::{GenericImage, GenericImageView};
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -27,15 +28,6 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         let video_stream_index = input.index();
         let mut decoder = context_decoder.decoder().video()?;
-        let mut scaler = Context::get(
-            decoder.format(),
-            decoder.width(),
-            decoder.height(),
-            ffmpeg::format::Pixel::RGB24,
-            decoder.width(),
-            decoder.height(),
-            Flags::BILINEAR,
-        )?;
         let packets: Vec<_> = ictx.packets().collect();
 
         for (stream, packet) in packets {
@@ -43,8 +35,7 @@ fn main() -> Result<(), Box<dyn Error>> {
                 decoder.send_packet(&packet)?;
                 let mut decoded = Video::empty();
                 while decoder.receive_frame(&mut decoded).is_ok() {
-                    let mut rgb_frame = Video::empty();
-                    process_frame(&mut rgb_frame, i, &mut final_frame)?;
+                    process_frame(&mut decoded, i, &mut final_frame)?;
                     break;
                 }
             }
@@ -61,12 +52,66 @@ fn process_frame(
     index: usize,
     final_frame: &mut Option<image::ImageBuffer<image::Rgb<u8>, Vec<u8>>>,
 ) -> Result<(), Box<dyn Error>> {
-    let img = image::ImageBuffer::<image::Rgb<u8>, Vec<u8>>::from_raw(
+    let mut scaler = Context::get(
+        frame.format(),
         frame.width(),
         frame.height(),
-        frame.data(0).to_vec(),
-    )
-    .unwrap();
+        ffmpeg::format::Pixel::RGB24,
+        frame.width(),
+        frame.height(),
+        Flags::BILINEAR,
+    )?;
 
+    let mut rgb_frame = Video::empty();
+    scaler.run(frame, &mut rgb_frame)?;
+    println!(
+        "Frame dimensions: {}x{}",
+        rgb_frame.width(),
+        rgb_frame.height()
+    );
+    println!("rgb_frame format: {:?}", rgb_frame.format());
+    println!("Data length: {}", rgb_frame.data(0).len());
+
+    let img = match image::ImageBuffer::<image::Rgb<u8>, Vec<u8>>::from_raw(
+        rgb_frame.width(),
+        rgb_frame.height(),
+        rgb_frame.data(0).to_vec(),
+    ) {
+        Some(img) => img,
+        None => {
+            return Err(Box::new(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                "Failed to create image buffer",
+            )))
+        }
+    };
+
+    let (width, height) = img.dimensions();
+    let quarter_width = width / 2;
+    let quarter_height = height / 2;
+
+    if final_frame.is_none() {
+        *final_frame = Some(image::ImageBuffer::new(width, height));
+    }
+
+    let quarter = match index {
+        0 => img.view(0, 0, quarter_width, quarter_height).to_image(),
+        1 => img
+            .view(quarter_width, 0, quarter_width, quarter_height)
+            .to_image(),
+        2 => img
+            .view(0, quarter_height, quarter_width, quarter_height)
+            .to_image(),
+        _ => img
+            .view(quarter_width, quarter_height, quarter_width, quarter_height)
+            .to_image(),
+    };
+    if let Some(ref mut final_img) = final_frame {
+        final_img.copy_from(
+            &quarter,
+            (index as u32 % 2) * quarter_width,
+            (index as u32 / 2) * quarter_height,
+        )?;
+    }
     Ok(())
 }
